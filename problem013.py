366b733b0477235ca78545bad7ca724ae093251c
DSShin <jeramis@gmail.com>
2015-08-12 15:09:09 +0900

    8/12 3pm

--- problem013.py
+++ problem013.py
@@ -1,3 +1,32 @@
 __author__ = 'USER'
 
+start = 1
+longstart = 1
+longcount = 1
+for i in range(1,1000000):
+    start = i
+    if i == 1 :
+        print (1, "Count is : 1")
+    else:
+        print (i, " -> ", end='')
+    count = 1
+    while(i!=1):
+        if i%2 == 0 :
+            i = int(i/2)
+            if i == 1 :
+                count = count + 1
+                print(i, "Count is : ", count)
+            else:
+                print (i, " -> ", end='')
+                count = count + 1
+        else :
+            i = i*3 + 1
+            print (i, " -> ", end='' )
+            count = count + 1
 
+    if count > longcount :
+        longstart = start
+        longcount = count
+    print("-----------------------------------------------------------------------------------------------")
+
+print ("Longest chain Staring is ", longstart)

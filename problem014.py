c74b0a853f91c0088e3caddb7db253d8cee4107e
DSShin <jeramis@gmail.com>
2015-08-12 17:58:16 +0900

    zxc

--- problem014.py
+++ problem014.py
@@ -1,18 +1,8 @@
 __author__ = 'USER'
 
-i = 0
-j = 2
+road = 1
+for i in range(1,21):
+    road = road * i**2
 
-count = 0
-route = 0
-while True :
-    for k in (1)
-        if k == 0 :
-            i = i + 1
-            if i == 2 :
-
-        if k == 1 :
-            j = j + 1
-
-    if i == 2 and j == 2 :
+print (road+2)
 

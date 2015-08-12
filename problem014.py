5a10751a1c0bb389777a4fefa6158997aed54eb9
DSShin <jeramis@gmail.com>
2015-08-12 15:23:57 +0900

    rename

---
+++ problem014.py
@@ -0,0 +1,18 @@
+__author__ = 'USER'
+
+i = 0
+j = 2
+
+count = 0
+route = 0
+while True :
+    for k in (1)
+        if k == 0 :
+            i = i + 1
+            if i == 2 :
+
+        if k == 1 :
+            j = j + 1
+
+    if i == 2 and j == 2 :
+
